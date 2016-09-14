527de793e64bba13ccab0e5a639630524c6c0325
archie <archierombo@gmail.com>
2016-09-14 07:37:51 +0300

    update

---
+++ app/src/main/java/com/archieowinorombo/sharedpreferences/MainActivity.java
@@ -0,0 +1,43 @@
+package com.archieowinorombo.sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText usernameInput,passwordInput;
+    TextView archiesText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        archiesText = (TextView) findViewById(R.id.archiesText);
+    }
+    //save the users info
+    public void saveInfo(View view){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("username",usernameInput.getText().toString());
+        editor.putString("password",passwordInput.getText().toString());
+        editor.apply();
+        Toast.makeText(this,"Saved!",Toast.LENGTH_LONG).show();
+
+    }
+    //print out out the saved data
+    public void displayData(View view){
+        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String name = sharedPref.getString("username","");
+        String pw = sharedPref.getString("password","");
+        archiesText.setText(name+ " "+pw);
+    }
+
+}
